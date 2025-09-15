53f22f57889a5c0b3cc66ef09461fba42377e216
Sabbir Hossain <sabbir.cse.hossain@gmail.com>
2025-09-15 11:29:01 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import Banner from "./components/Banner/Banner"
+import Blog from "./components/Blog/Blog"
+import Contact from "./components/Contact/Contact"
+import Copy from "./components/Copy/Copy"
+import Footer from "./components/Footer/Footer"
+import Header from "./components/Header/Header"
+import Learn from "./components/Learn/Learn"
+import Logo from "./components/Logo/Logo"
+import Maap from "./components/Maap/Maap"
+import Navbar from "./components/Navbar/Navbar"
+import Services from "./components/Services/Services"
+import ImageSlider from "./components/Slider/ImageSlider"
+import Slider from "./components/Slider/Slider"
+import Supplier from "./components/Supplier/Supplier"
+
+
+function App() {
+
+
+  return (
+    <>
+      <Header></Header>
+      <Navbar></Navbar>
+      <Banner></Banner>
+      <Supplier></Supplier>
+      {/* <Slider></Slider> */}
+      <ImageSlider></ImageSlider>
+      <Services></Services>
+      <Learn></Learn>
+      <Logo></Logo>
+      <Blog></Blog>
+      <Maap></Maap>
+      <Contact></Contact>
+      <Footer></Footer>
+      <Copy></Copy>
+      
+      
+
+
+    </>
+  )
+}
+
+export default App
